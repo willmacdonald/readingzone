b1a8d63df2fad15f5a2ab694d13d6dc41ca687b7
William Macdonald <will.macdonald@gmail.com>
2019-09-26 16:45:05 +0200

    book details page with related news

--- src/Controller/BookDetailsController.php
+++ src/Controller/BookDetailsController.php
@@ -25,28 +25,49 @@ class BookDetailsController extends AbstractController
         $stmt->execute(['isbn' => $isbn]);
         $books_details = $stmt->fetch();
 
-        /*
+        /**
          * related title
          */
 
-        $sql = 'SELECT ISBN13 FROM related_titles WHERE ISBN13 = :isbn LIMIT 1';
+        $sql = 'SELECT TI, FN, ISBN13 FROM book_details WHERE FN LIKE :FN AND ISBN != :isbn LIMIT 4 ';
         $stmt = $connection->prepare($sql);
-        $stmt->execute(['isbn' => $isbn]);
+        $stmt->execute(
+            ['isbn' => $isbn,
+            'FN' => $books_details['FN'],
+            ]);
         $related_titles = $stmt->fetchAll();
 
-        /*
+        /**
          * related articles
+         *
          */
 
-        $sql = 'SELECT ISBN13 FROM related_articles WHERE ISBN13 = :isbn LIMIT 1';
+
+        $sql = 'SELECT id, headline FROM news WHERE isbn = :isbn OR headline like :FN OR body like :FN OR 
+                                    headline like :TI OR body like :TI ORDER BY mark LIMIT 6';
         $stmt = $connection->prepare($sql);
-        $stmt->execute(['isbn' => $isbn]);
-        $related_articles = $stmt->fetchAll();
+        $stmt->execute(
+            [   'isbn' => $isbn,
+                'FN' => $books_details['FN'],
+                'TI' => $books_details['TI'],
+            ]);
+        $related_articles[] = $stmt->fetchAll();
+        if (count($related_titles) > 1) {
+            foreach ($related_titles as $related_title) {
+                $stmt->execute(
+                    [   'isbn' => $related_title['ISBN13'],
+                        'FN' => $related_title['FN'],
+                        'TI' => $related_title['TI'],
+                    ]);
+                $related_articles[] = $stmt->fetchAll();
+            }
+        }
 
         return $this->render('book-details.html.twig', [
             'book_details' => $books_details,
             'related_titles' => $related_titles,
-            'related_articles' => $related_articles,
+            'related_articles' => array_flatten($related_articles),
         ]);
+
     }
 }
